fe510233d6148da2efa224be045246947580f91b
zainab-ibrahim <163460671+zainab-ibrahim@users.noreply.github.com>
2024-03-31 14:34:10 -0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,59 @@
+let fruitName = "mango";
+
+console.log(fruitName)
+
+function logFruitName(){
+    console.log(fruitName)
+}
+
+logFruitName()
+
+let fruitName1 = "banana"
+
+function logFruitName1(){
+    console.log(fruitName1)
+}
+
+logFruitName1()
+
+let carName = "Toyota"
+
+function logCarName(){
+    console.log(carName)
+}
+
+logCarName()
+
+//example1
+
+let productName = "Inkpen"
+
+function logproductName(){
+console.log(productName)
+}
+
+logproductName()
+
+//example 02
+
+// HalfFry
+// 1Egg, 1.5 Butter ,2 salt
+
+function halfFry(salt, butter){
+    let coocked=salt + butter +1
+    return coocked
+}
+console.log(halfFry(5,8))
+
+halfFry(1 , 2)
+halfFry(3 , 5)
+
+//example 03
+
+// let dishesNames = "biryani", ""
+
+function logDishesNames(desi, chinese, BBQ) {
+    console.log(desi, chinese, BBQ);
+}
+logDishesNames("biryani", "chowmien", "malaiBoti");
+
